c5d241350acc1f093c030d7b1b53a2856cded4ca
secpen <103785976+secpen@users.noreply.github.com>
2022-07-07 15:24:55 -0400

    Update linuxsetupvm.sh

--- linuxsetupvm.sh
+++ linuxsetupvm.sh
@@ -5,3 +5,11 @@ apt-get install lxde -y
 apt-get install xrdp -y
 DEBIAN_FRONTEND=noninteractive
 /etc/init.d/xrdp start
+git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
+git clone https://github.com/ExAndroidDev/impacket.git
+cd impacket
+git checkout ntlmrelayx-adcs-attack
+apt install python3-venv
+python3 -m venv impacket
+source impacket/bin/activate
+pip install .
