2b2d553f7b7285059c57f333607c38845565d014
secpen <103785976+secpen@users.noreply.github.com>
2022-07-19 11:28:01 -0400

    Update linuxsetupvm.sh

--- linuxsetupvm.sh
+++ linuxsetupvm.sh
@@ -1,9 +1,9 @@
 #/bin/sh
-apt-get update
-apt-get upgrade
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update
+apt-get -y upgrade
 apt-get install lxde -y
 apt-get install xrdp -y
-DEBIAN_FRONTEND=noninteractive
 /etc/init.d/xrdp start
 git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
 git clone https://github.com/ExAndroidDev/impacket.git
